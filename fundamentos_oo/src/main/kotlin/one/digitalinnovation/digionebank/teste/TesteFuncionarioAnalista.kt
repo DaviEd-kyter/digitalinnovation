db92a7c1469fb87dcc8a1364932987aadd94e6d1
DaviEd <davi_eduardinho@outlook.com>
2022-08-01 16:02:44 -0300

    Created tests and report printer (Criação de testes e impressão de relatórios)

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/teste/TesteFuncionarioAnalista.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank.teste
+
+import one.digitalinnovation.digionebank.Analista
+
+fun main() {
+    val eduardo = Analista("Eduardo Davi", "1234567899", 5000.0)
+    ImprimeRelatorioFuncionario.imprime(eduardo)
+
+}
---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/teste/TesteFuncionarioGerente.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank.teste
+
+import one.digitalinnovation.digionebank.Gerente
+
+fun main() {
+    val neli = Gerente("Neli Pereira", "2564456499", 9000.0)
+    ImprimeRelatorioFuncionario.imprime(neli)
+
+}
